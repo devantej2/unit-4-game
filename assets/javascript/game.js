74d4a0d9e2f22a0fc7b737397f978b5e1657ec23
Devante <devante.jones229@gmail.com>
2019-10-20 23:52:36 -0400

    created array for images to display through jQuery, added some styling to css

---
+++ assets/javascript/game.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+
+    let wins = 0;
+    let losses = 0;
+    let totalScore = 0;
+    let userScore = 0;
+    const targetNumber = Math.floor(Math.random() * (101) + 19); //target number calculated
+   // console.log(targetNumber);
+
+    let red = "../unit-4-game/assets/images/crystal_red.png";
+    let blue = "../unit-4-game/assets/images/crystal_blue.png";
+    let green = "../unit-4-game/assets/images/crystal_green.png";
+    let purple = "../unit-4-game/assets/images/crystal_purple.png";
+    
+
+    $("#targetNumber").text(targetNumber);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+    $("#user-score").text(userScore);
+
+
+//create the crystal values
+    var crystalImages = [red, blue, green, purple];
+
+    for (let i = 0; i < 4; i++){
+        const randNum = Math.floor(Math.random() * 12) + 1;
+        const crystalPic = $("<img>");
+        crystalPic.addClass("crystal-color");
+        crystalPic.attr("src", crystalImages[i]);
+        crystalPic.attr("data-crystalValue",randNum);
+        $("#crystal").append(crystalPic);
+        
+    }
+    
+
+    $(".crystal-color").on("click", function(){
+        let value = ($(this).attr("data-crystalValue"));
+        value = parseInt(value);
+        userScore += value;
+        $("#user-score").text(userScore);
+        if (userScore === targetNumber) {
+            alert("You win!");
+            wins++;
+            $("#wins").text(wins);
+            
+
+      }else if (userScore >= targetNumber) {
+        alert("You lose!!");
+        losses++;
+        $("#losses").text(losses);
+        
+      }
+    });
+    
+    
+    
+    });
