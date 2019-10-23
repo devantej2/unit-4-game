34d983407998e2955c0ad6f1cb5868f140c37c28
Devante <devante.jones229@gmail.com>
2019-10-23 19:19:39 -0400

    Added javascript function to clear the data values after user wins or loses; added css styling as well

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     let wins = 0;
     let losses = 0;
-    let totalScore = 0;
     let userScore = 0;
-    const targetNumber = Math.floor(Math.random() * (101) + 19); //target number calculated
-   // console.log(targetNumber);
+    let targetNumber = Math.floor(Math.random() * (101) + 19); //target number calculated
 
     let red = "../unit-4-game/assets/images/crystal_red.png";
     let blue = "../unit-4-game/assets/images/crystal_blue.png";
     let green = "../unit-4-game/assets/images/crystal_green.png";
     let purple = "../unit-4-game/assets/images/crystal_purple.png";
-    
+
 
     $("#targetNumber").text(targetNumber);
     $("#wins").text(wins);
@@ -19,21 +17,21 @@ $(document).ready(function() {
     $("#user-score").text(userScore);
 
 
-//create the crystal values
-    var crystalImages = [red, blue, green, purple];
-
-    for (let i = 0; i < 4; i++){
-        const randNum = Math.floor(Math.random() * 12) + 1;
+    //create the crystal values
+    let crystalImages = [red, blue, green, purple];
+    for (let i = 0; i < 4; i++) {
+        let randNum = Math.floor(Math.random() * 12) + 1;
         const crystalPic = $("<img>");
         crystalPic.addClass("crystal-color");
         crystalPic.attr("src", crystalImages[i]);
-        crystalPic.attr("data-crystalValue",randNum);
+        crystalPic.attr("data-crystalValue", randNum);
         $("#crystal").append(crystalPic);
-        
     }
-    
 
-    $(".crystal-color").on("click", function(){
+
+
+
+    $(".crystal-color").on("click", function () {
         let value = ($(this).attr("data-crystalValue"));
         value = parseInt(value);
         userScore += value;
@@ -42,16 +40,27 @@ $(document).ready(function() {
             alert("You win!");
             wins++;
             $("#wins").text(wins);
-            
+            reset();
+        }
 
-      }else if (userScore >= targetNumber) {
-        alert("You lose!!");
-        losses++;
-        $("#losses").text(losses);
-        
-      }
+        else if (userScore >= targetNumber) {
+            alert("You lose!!");
+            losses++;
+            $("#losses").text(losses);
+            reset();
+        }
     });
-    
-    
-    
-    });
+    function reset() {
+        userScore = 0;
+        targetNumber = Math.floor(Math.random() * (101) + 19);
+        $(".crystal-color").each(function () {
+            let randNum = Math.floor(Math.random() * 12) + 1;
+            $(this).attr("data-crystalValue", randNum);
+        });
+        $("#user-score").text(userScore);
+        $("#targetNumber").text(targetNumber);
+
+    }
+
+
+});
